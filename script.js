f8e4db95d3d48a9e468ab5d2db9c462de07b7aea
angryzoopatron <81863211+angryzoopatron@users.noreply.github.com>
2021-06-16 14:03:29 -0400

    Update script.js
    Removed array, added filler functions, need to add comments so it makes sense when I view it later

--- script.js
+++ script.js
@@ -1,37 +1,79 @@
-let gameBoard = [
-    '1', '2', '3',
-    '4', '5', '6',
-    '7', '8', '9'
-];
+const gameStatus = document.querySelector('.game-status')
 
+let gameActive = true;
+let currentPlayer = 'X';
+let gameState = ['', '', '', '', '', '', '', '', ''];
+
+const winner = () => `${currentPlayer} has won!`;
+const draw = () => `DRAW!`
+const playerTurn = () => `It's ${currentPlayer}'s turn`;
+
+const tableContainer = document.createElement('div');
 const myHTML = `
-  <table>
+  <table id="center">
   <tr id="top-row">
-      <td data-cell-index="0" class="cell">${gameBoard[0]}</td>
-      <td data-cell-index="1" class="cell">${gameBoard[1]}</td>
-      <td data-cell-index="2" class="cell">${gameBoard[2]}</td>
+      <td data-cell-index="0" class="cell"></td>
+      <td data-cell-index="1" class="cell"></td>
+      <td data-cell-index="2" class="cell"></td>
   </tr>
   <tr id="mid-row">
-      <td data-cell-index="3" class="cell">${gameBoard[3]}</td>
-      <td data-cell-index="4" class="cell">${gameBoard[4]}</td>
-      <td data-cell-index="5" class="cell">${gameBoard[5]}</td>
+      <td data-cell-index="3" class="cell"></td>
+      <td data-cell-index="4" class="cell"></td>
+      <td data-cell-index="5" class="cell"></td>
   </tr>
   <tr id="bot-row">
-      <td data-cell-index="6" class="cell">${gameBoard[6]}</td>
-      <td data-cell-index="7" class="cell">${gameBoard[7]}</td>
-      <td data-cell-index="8" class="cell">${gameBoard[8]}</td>
+      <td data-cell-index="6" class="cell"></td>
+      <td data-cell-index="7" class="cell"></td>
+      <td data-cell-index="8" class="cell"></td>
   </tr>
   </table>
 `;
 
 const myFragment = document.createRange().createContextualFragment(myHTML);
-document.body.appendChild(myFragment);
+tableContainer.appendChild(myFragment);
+document.body.prepend(tableContainer);
 
-function handleCellClick(e) {
+gameStatus.innerHTML = playerTurn();
+
+function handleCell(clickedCell, clickedIndex) {
+    gameState[clickedIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerTurn() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    gameStatus.innerHTML = currentPlayer;
+}
+
+function handleResults() {
+
+}
+
+function handleClick(e) {
+    console.log('clicked!')
     const clickedCell = e.target;
 
-    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+    const clickIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickIndex] !== "" || !gameActive) return;
+
+    handleCell(clickedCell, clickIndex);
+    handleResults();
 }
+
+function handleRestart(e) {
+    gameActive = true;
+    currentPlayer = 'X';
+    gameState = ['', '', '', '', '', '', '', '', ''];
+
+    gameStatus.innerHTML = playerTurn();
+
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleClick));
+document.querySelector('.restart').addEventListener('click', handleRestart);
+
 
 // https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn
 
